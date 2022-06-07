86292a190fcc67b61b5fb5c60207687f1a634a5a
Tim Statler <62028706+timstats91@users.noreply.github.com>
2022-06-07 09:37:40 -0500

    PI calls with AXIOS

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -7,25 +7,25 @@
 
 <script>
 export default {
-  name: "EventCard",
+  name: 'EventCard',
   props: {
     event: Object,
   },
-};
+}
 </script>
 
 <style scoped>
-  .event-card {
-    padding: 20px;
-    width: 250px;
-    cursor: pointer;
-    border: 1px solid #39495c;
-    margin-bottom: 18px;
-    transition: 250ms;
-  }
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+  transition: 250ms;
+}
 
-  .event-card:hover {
-    transform: scale(1.01);
-    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-  }
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require("@vue/cli-service");
+/* eslint-disable prettier/prettier */
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-});
+})
